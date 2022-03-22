7d21ae803c59e29451c45cd702a3c7b7c6d08889
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2022-03-22 18:30:53 -0300

    faz o 6 e 7

--- src/insights.py
+++ src/insights.py
@@ -13,21 +13,12 @@ def get_unique_job_types(path):
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
+    filteredJobs = [
+        job
+        for job in jobs if job['job_type'] == job_type
+    ]
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    return []
+    return filteredJobs
 
 
 def get_unique_industries(path):
@@ -42,21 +33,12 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
+    filteredJobs = [
+        job
+        for job in jobs if job['industry'] == industry
+    ]
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    return filteredJobs
 
 
 def get_max_salary(path):
