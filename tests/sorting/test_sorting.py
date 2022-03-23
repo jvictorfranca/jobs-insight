acace2bfd69ae46a1c963be6afa62901235c3737
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2022-03-23 16:10:41 -0300

    tesnta o 10

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,169 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+import pytest
 
 
-def test_sort_by_criteria():
-    pass
+@pytest.fixture
+def mockedJobs():
+    return [
+        {
+            "min_salary": 1000,
+            "max_salary": 2000,
+            "date_posted": "2020-07-25",
+        },
+        {
+            "min_salary": 3000,
+            "max_salary": 4000,
+            "date_posted": "2020-04-27",
+        },
+        {
+            "min_salary": 2500,
+            "max_salary": 5000,
+            "date_posted": "2019-04-25",
+        },
+        {
+            "max_salary": 2005,
+            "date_posted": "2020-04-26",
+        },
+        {
+            "min_salary": 1010,
+            "date_posted": "2020-04-24",
+        },
+        {
+            "max_salary": 2001,
+            "date_posted": "2020-04-23",
+        },
+        {
+            "min_salary": 900,
+            "date_posted": "2020-04-25",
+        },
+        {
+            "min_salary": 950,
+            "max_salary": 1800,
+        },
+    ]
+
+
+jobs_sorted_by_min = [
+        {
+            "min_salary": 900,
+            "date_posted": "2020-04-25",
+        },
+        {
+            "min_salary": 950,
+            "max_salary": 1800,
+        },
+        {
+            "min_salary": 1000,
+            "max_salary": 2000,
+            "date_posted": "2020-07-25",
+        },
+        {
+            "min_salary": 1010,
+            "date_posted": "2020-04-24",
+        },
+        {
+            "min_salary": 2500,
+            "max_salary": 5000,
+            "date_posted": "2019-04-25",
+        },
+        {
+            "min_salary": 3000,
+            "max_salary": 4000,
+            "date_posted": "2020-04-27",
+        },
+        {
+            "max_salary": 2005,
+            "date_posted": "2020-04-26",
+        },
+        {
+            "max_salary": 2001,
+            "date_posted": "2020-04-23",
+        },
+    ]
+
+
+jobs_sorted_by_max = [
+        {
+            "min_salary": 2500,
+            "max_salary": 5000,
+            "date_posted": "2019-04-25",
+        },
+        {
+            "min_salary": 3000,
+            "max_salary": 4000,
+            "date_posted": "2020-04-27",
+        },
+        {
+            "min_salary": 1000,
+            "max_salary": 2000,
+            "date_posted": "2020-07-25",
+        },
+        {
+            "max_salary": 2005,
+            "date_posted": "2020-04-26",
+        },
+        {
+            "max_salary": 2001,
+            "date_posted": "2020-04-23",
+        },
+        {
+            "min_salary": 950,
+            "max_salary": 1800,
+        },
+        {
+            "min_salary": 1010,
+            "date_posted": "2020-04-24",
+        },
+        {
+            "min_salary": 900,
+            "date_posted": "2020-04-25",
+        },
+    ]
+
+jobs_sorted_by_date = [
+        {
+            "min_salary": 1000,
+            "max_salary": 2000,
+            "date_posted": "2020-07-25",
+        },
+        {
+            "min_salary": 3000,
+            "max_salary": 4000,
+            "date_posted": "2020-04-27",
+        },
+        {
+            "max_salary": 2005,
+            "date_posted": "2020-04-26",
+        },
+        {
+            "min_salary": 900,
+            "date_posted": "2020-04-25",
+        },
+        {
+            "min_salary": 1010,
+            "date_posted": "2020-04-24",
+        },
+        {
+            "max_salary": 2001,
+            "date_posted": "2020-04-23",
+        },
+        {
+            "min_salary": 2500,
+            "max_salary": 5000,
+            "date_posted": "2019-04-25",
+        },
+        {
+            "min_salary": 950,
+            "max_salary": 1800,
+        },
+    ]
+
+
+def test_sort_by_criteria(mockedJobs):
+    sort_by(mockedJobs, "min_salary")
+    assert mockedJobs == jobs_sorted_by_min
+    sort_by(mockedJobs, "max_salary")
+    assert mockedJobs == jobs_sorted_by_max
+    sort_by(mockedJobs, "date_posted")
+    assert mockedJobs == jobs_sorted_by_date
+
